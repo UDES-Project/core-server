c759747a91f967b250aba72612759ec14d1a878e
Alex Zalo <zalo.alex56@gmail.com>
2024-02-02 23:31:23 +0100

    add cors

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flash_flask import App
 from flash_flask.db import MySQL
 
+from flask_cors import CORS
+
 import os
 import dotenv
 
 dotenv.load_dotenv()
 
 app = App(__name__)
+
+CORS(app.flask)
 
 MySQL.init(os.getenv("MYSQL_HOST"), os.getenv("MYSQL_PORT"), os.getenv("MYSQL_USER"), os.getenv("MYSQL_PASSWORD"), "umes")
 
