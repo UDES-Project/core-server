4331758ba7ea316698095aa69d04ff15d340d719
Alex Zalo <zalo.alex56@gmail.com>
2024-02-04 19:21:14 +0100

    fix bad content base64 error

--- routes/api/message/decrypt/decrypt.py
+++ routes/api/message/decrypt/decrypt.py
@@ -8,12 +8,11 @@ import base64
 def endpoint():
     try:
         content, key = get_url_args([("content", str), ("key", str)])
+        content, key = base64.b64decode(content.encode()), key.encode()
     except Exception as e:
         return {
             "error": str(e)
         }
-    
-    content, key = base64.b64decode(content.encode()), key.encode()
     
     result = "".join([chr(char ^ key[i % len(key)]) for i, char in enumerate(content)])
     
