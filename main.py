09a6b91fc003cd8dd430ea1ead3ba5845637ebdd
Alex Zalo <zalo.alex56@gmail.com>
2024-11-25 19:35:45 +0100

    Remove useless logger

--- main.py
+++ main.py
@@ -5,21 +5,10 @@ from flask_cors import CORS
 
 import os
 import dotenv
-import logging
 
 dotenv.load_dotenv()
 
 app = App(__name__)
-
-handler = logging.FileHandler("logs/flask.log")
-handler.setLevel(logging.INFO)
-
-formatter = logging.Formatter(
-    "[%(asctime)s] %(levelname)s in %(module)s: %(message)s"
-)
-handler.setFormatter(formatter)
-
-app.flask.logger.addHandler(handler)
 
 CORS(app.flask)
 
