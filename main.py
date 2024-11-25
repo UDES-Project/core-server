2e463fb544a82dcfbfb26feca7de9b784bbe1661
Alex Zalo <zalo.alex56@gmail.com>
2024-11-25 20:15:45 +0100

    Add message expiration

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from flash_flask import App
 from flash_flask.db import MySQL
 
@@ -14,5 +17,13 @@ CORS(app.flask)
 
 MySQL.init(os.getenv("MYSQL_HOST"), os.getenv("MYSQL_PORT"), os.getenv("MYSQL_USER"), os.getenv("MYSQL_PASSWORD"), os.getenv("MYSQL_DATABASE"))
 
+def delete_expired_messages():
+    MySQL.delete("messages", "created_at < %s", (datetime.now() - timedelta(days=7),))
+
 if __name__ == "__main__":
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=delete_expired_messages, trigger="interval", hours=24)
+    scheduler.start()
+
     app.run(debug=True)
